6836026a5c50a7a3343faba9ea5a4e8f79f0e7f2
wpxdartmouth <61171981+wpxdartmouth@users.noreply.github.com>
2020-05-25 15:19:07 -0700

    font

--- app/src/main/java/com/example/echo/CreateActivity.java
+++ app/src/main/java/com/example/echo/CreateActivity.java
@@ -4,20 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreateActivity extends AppCompatActivity {
-
+    TextView txTitle;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
-        TextView txTitle = (TextView)findViewById(R.id.note_title);
-        TextView txText = (TextView)findViewById(R.id.note_text);
+        txTitle = (TextView)findViewById(R.id.note_title);
+       editText = (EditText)findViewById(R.id.note_text);
         Typeface titleFont = Typeface.createFromAsset(getAssets(), "font/title.ttf");
         Typeface textFont = Typeface.createFromAsset(getAssets(), "font/text.ttf");
         txTitle.setTypeface(titleFont);
-        txText.setTypeface(textFont);
+        editText.setTypeface(textFont);
+    }
+
+    public void onEchoClicked(View v) {
+        String text = editText.getText().toString();
     }
 }
